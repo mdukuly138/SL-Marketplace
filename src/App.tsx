0e3081594be7d91dfbed48a95a69c915f1404ed2
mdukuly138 <mdukuly138@gmail.com>
2026-08-08 19:48:34 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './hooks/useAuth'
 import { AppLayout } from './layouts/AppLayout'
 import { Home } from './pages/Home'
 import { Market } from './pages/Market'
@@ -7,20 +8,28 @@ import { Messages } from './pages/Messages'
 import { Profile } from './pages/Profile'
 import { ListingDetails } from './pages/ListingDetails'
 import { SellerProfile } from './pages/SellerProfile'
+import { SignUp } from './pages/SignUp'
+import { Login } from './pages/Login'
+import { VerifyEmail } from './pages/VerifyEmail'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/market" element={<Market />} />
-        <Route path="/sell" element={<Sell />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/listing/:id" element={<ListingDetails />} />
-        <Route path="/seller/:id" element={<SellerProfile />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/market" element={<Market />} />
+          <Route path="/sell" element={<Sell />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/listing/:id" element={<ListingDetails />} />
+          <Route path="/seller/:id" element={<SellerProfile />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/verify" element={<VerifyEmail />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   )
 }
 
